81d2e5df93e7ad379510fb16570cf1bff49c2149
Josemi H <jmhernandez.naranjo@gmail.com>
2025-11-19 01:13:03 +0000

    quiero que las imágenes no se carguen desde los  dominios (images.unspla

--- next.config.ts
+++ next.config.ts
@@ -16,32 +16,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'storage.googleapis.com',
-        port: '',
-        pathname: '/**',
-      }
-    ],
+    remotePatterns: [],
   },
 };
 
